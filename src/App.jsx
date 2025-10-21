ef240c6ef937be69eb02e8be5059c07645bc1b66
msDospako <fjochoa.ingenieria@gmail.com>
2025-10-21 17:47:44 -0600

    Initial clean setup: React + Vite + Tailwind base for Lechera22

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gray-50 text-gray-900'>
+      <h1 className='text-2xl font-semibold'>Sistema de Control de Inventarios Lechera 22 Amigos</h1>
+    </div>
+  );
+}
+export default App;
